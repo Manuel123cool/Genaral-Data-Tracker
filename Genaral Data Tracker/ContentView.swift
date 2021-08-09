7039bca9a6f78a43856e41ee044f63866e1b40c4
Manuel Kümpel <manuel-kuempel@web.de>
2021-08-09 21:32:37 +0200

    Initial Commit

---
+++ Genaral Data Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Genaral Data Tracker
+//
+//  Created by Manuel Kümpel on 09.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Genaral Data Tracker/Genaral_Data_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Genaral_Data_TrackerApp.swift
+//  Genaral Data Tracker
+//
+//  Created by Manuel Kümpel on 09.08.21.
+//
+
+import SwiftUI
+
+@main
+struct Genaral_Data_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
